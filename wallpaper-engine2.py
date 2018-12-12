1d73123243331d08c57cc6bf3f7431501100d687
Rafael Schreiber <raf.schreiber@gmail.com>
2018-12-12 15:27:33 +0100

    Added main program

---
+++ wallpaper-engine2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import sys
+import os
+import time
+from random import *
+
+try:
+    interval = int(sys.argv[1])
+    while True:
+        if interval <= 0:
+            print("Please type in a number greate than 0\n")
+            continue
+        else:
+            break
+except:
+    print("Type in an update interval (in sec)")
+    while True:
+        try:
+            interval = int(input(">>> "))
+            if interval <= 0:
+                print("Please type in a number greate than 0\n")
+                continue
+            else:
+                break
+        except ValueError:
+            print("Please type in a number\n")
+    
+
+
+while True:
+    while True:
+        randomnumber = randint(1, 99999)
+        link = "https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-" + str(randomnumber) +".jpg"
+        if os.system("wget -O /tmp/" + str(randomnumber) + ".jpg " + link) == 0:
+            break
+        else:
+            continue
+    pathtofile = "/tmp/" + str(randomnumber) + ".jpg"
+    cmd = "osascript -e 'tell application \"Finder\" to set desktop picture to POSIX file \"" + pathtofile + "\"'"
+    os.system(cmd)
+    time.sleep(interval)
